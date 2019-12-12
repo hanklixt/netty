dfa9b5b38ad5a35bd7c690e8ba86b7dd27a0edb5
hankli <1033110657>
2019-12-12 10:43:56 +0800

    FileChannel+Buffer读文件

--- src/main/java/com/hank/netty/nio/FileChannelTest02.java
+++ src/main/java/com/hank/netty/nio/FileChannelTest02.java
@@ -1,11 +1,28 @@
 package com.hank.netty.nio;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @author lxt
  * @date 2019-12-12-10:25
  */
 public class FileChannelTest02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final File file = new File("E:\\file01.txt");
 
+        final FileInputStream fileInputStream = new FileInputStream(file);
+
+        final FileChannel channel = fileInputStream.getChannel();
+
+        final ByteBuffer byteBuffer = ByteBuffer.allocate((int) file.length());
+
+        channel.read(byteBuffer);
+
+        fileInputStream.close();
+
+        System.out.println(new String(byteBuffer.array(), "utf-8"));
     }
 }
