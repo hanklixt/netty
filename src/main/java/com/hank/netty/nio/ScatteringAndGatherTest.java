2b6dba893ca6a0aed8fe2d49c7ed3596feeabad1
hankli <1033110657>
2019-12-19 15:37:34 +0800

    使用多个buffer将客户端数据读取完成，和从多个buffer读取数据合并

--- src/main/java/com/hank/netty/nio/ScatteringAndGatherTest.java
+++ src/main/java/com/hank/netty/nio/ScatteringAndGatherTest.java
@@ -1,6 +1,7 @@
 package com.hank.netty.nio;
 
 import java.net.InetSocketAddress;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
@@ -20,21 +21,21 @@ public class ScatteringAndGatherTest {
         byteBuffers[1] = ByteBuffer.allocate(3);
         final SocketChannel accept = socketChannel.accept();
         int readLength = 8;
+        long read = 0;
         while (true) {
-            long read = 0;
             while (read < readLength) {
                 read = accept.read(byteBuffers);
-                Arrays.asList(byteBuffers).stream().map(x ->
+                Arrays.stream(byteBuffers).map(x ->
                         "position:" + x.position() + ",limit" + x.limit()
                 ).forEach(System.out::println);
-                Arrays.asList(byteBuffers).forEach(buffer -> {
-                    buffer.flip();
-                });
+                Arrays.asList(byteBuffers).forEach(Buffer::flip);
                 long writeLength = 0;
                 while (writeLength < readLength) {
                     writeLength = accept.write(byteBuffers);
                     System.out.println("读取到的数据长度:" + writeLength);
                     Arrays.asList(byteBuffers).forEach(byteBuffer -> System.out.println(new String(byteBuffer.array())));
+                    byteBuffers[0].clear();
+                    byteBuffers[1].clear();
                 }
             }
 
