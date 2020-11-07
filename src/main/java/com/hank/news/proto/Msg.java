4d0a7e987541bc180e99f56ceb68c6177c8e9230
hanklixt <asdv32nihaoxt>
2020-11-07 14:47:02 +0800

    ---测试相关

---
+++ src/main/java/com/hank/news/proto/Msg.java
@@ -0,0 +1,17 @@
+package com.hank.news.proto;
+
+import lombok.Data;
+
+/**
+ * @author lxt
+ * @create 2020-11-07 11:19
+ */
+@Data
+public class Msg {
+
+    private int id;
+
+    private String msg;
+
+
+}
